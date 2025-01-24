43fe5bf03038c4ba4de1320e986a80dfaf776027
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-24 20:50:36 +0300

    navigation menu created

---
+++ components/navigation-menu.js
@@ -0,0 +1,24 @@
+import { LitElement, html, css } from 'lit';
+
+export class NavigationMenu extends LitElement {
+  static styles = css`
+    nav {
+      display: flex;
+      gap: 1rem;
+    }
+    a {
+      text-decoration: none;
+      color: blue;
+    }
+  `;
+
+  render() {
+    return html`
+      <nav>
+        <a href="/">Employee List</a>
+        <a href="/add">Add Employee</a>
+      </nav>
+    `;
+  }
+}
+customElements.define('navigation-menu', NavigationMenu);
