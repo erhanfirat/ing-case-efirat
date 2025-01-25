653770ef02038b93773a9231f3109575055e6fd8
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-25 20:40:56 +0300

    stateLogger description added

--- store/middleware/state-logger.js
+++ store/middleware/state-logger.js
@@ -1,3 +1,7 @@
+/**
+ * It is regular logger for all redux actions and state changes
+ * redux-logger is not used because of a bug on it
+ */
 export const stateLogger = (store) => (next) => (action) => {
   console.log('[Middleware] Prev state: ', store.getState());
   console.log('[Middleware] Action: ', action);
